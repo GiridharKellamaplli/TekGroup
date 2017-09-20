3a4bd857b9aebc98c156d5d6ecbea5bf3347fc63
GiridharKellampalli <giri.kellampalli@gmail.com>
2017-09-20 17:29:44 -0400

    TekTimeSheet: Initial Commit - Project Set Up

---
+++ src/main/java/com/tekgroup/tektimesheet/TekTimeSheetApplication.java
@@ -0,0 +1,12 @@
+package com.tekgroup.tektimesheet;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TekTimeSheetApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TekTimeSheetApplication.class, args);
+	}
+}
